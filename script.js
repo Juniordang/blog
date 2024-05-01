c1f99cbac5bf88a423c5e33efd2e62d1d7101bf9
Junior dange <dangejunior00@gmail.com>
2024-05-01 12:37:31 -0300

    ajuste no botão do menu e nos texto do article

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const menu = document.querySelector('.c-menu')
-const button = document.getElementById('btn')
-button.addEventListener('click', function() {
-    if (menu.style.display === 'none') {
+function clickMenu() {
+    if(menu.style.display === 'none') {
         menu.style.display = 'block'
     }else {
         menu.style.display = 'none'
     }
-})
+}
